d7a51fa2b412e9ca9072ecc82ed716b0814a9b51
Hara-kun <is0575ve@ed.ritsumei.ac.jp>
2022-04-15 18:04:24 +0900

    initial commit

--- move.py
+++ move.py
@@ -2,40 +2,45 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Int32
+from camera_opencv.msg import PositionValues
 import time
 
 class Movement():
     def __init__(self):
         self.pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size = 1)
+        self.sub = rospy.Subscriber('/RLDict', PositionValues, self.sub)
+        self.camera_data = PositionValues()
+        self.camera_data.up_down = 0
+        self.camera_data.left_right = 1
+        self.camera_data.far_near = 2
+        self.move_on = 0   #moveOn_distance
+        self.theta = 0     #rotate_theta
 
-    def move_right(self):
-        theta = 1
-        speed = 20
-        target_time = theta / speed
+    def move_go(self):
+        speed = 0.1
+        target_time = self.move_on / 10
 
         t = Twist()
-        t.linear.x = 0
-        t.angular.z = (-1)*speed*3.14/180
+        t.linear.x = (-1) * self.move_on * speed
+        t.angular.z = 0
 
         start_time = time.time()
         end_time = time.time()
 
-        rate = rospy.Rate(10)
+        rate = rospy.Rate(50)
 
         while end_time - start_time <= target_time:
             self.pub.publish(t)
             end_time = time.time()
             rate.sleep()
 
-    def move_left(self):
-        theta = 1
+    def move_turn(self):
         speed = 20
-        target_time = theta / speed
+        target_time = self.theta / speed
 
         t = Twist()
         t.linear.x = 0
-        t.angular.z = speed*3.14/180
+        t.angular.z = self.theta*speed*3.14/180
 
         start_time = time.time()
         end_time = time.time()
@@ -48,17 +53,28 @@ class Movement():
             rate.sleep()
 
     def sub(self, message):
-        if message.data == 0:
-            self.move_right()
-        elif message.data == 2:
-            self.move_left()
-        #elif message.data == 1:
-            #stop
+        if message.left_right == 0:
+            self.theta = -1
+        elif message.left_right == 2:
+            self.theta = 1
+        else:
+            self.theta = 0
+        print("me " + str(message.left_right))
+        print("tu " + str(self.theta))
+        self.move_turn()
+
+        if message.far_near == 0:
+            self.move_on = 2
+        elif message.far_near == 1:
+            self.move_on = 1
+        else:
+            self.move_on = 0
+        print("go " + str(self.move_on))
+        self.move_go()
 
 if __name__ == '__main__':
     rospy.init_node('move_operation')
     movement = Movement()
-    sub = rospy.Subscriber('/RLDict', Int32, movement.sub)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         rate.sleep()
