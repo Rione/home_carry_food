5bf94d21e0326ffcf67ebdb0c61d7f484b02039b
Hara-kun <is0575ve@ed.ritsumei.ac.jp>
2022-05-20 17:47:14 +0900

    ver.1.1

--- move.py
+++ move.py
@@ -4,6 +4,7 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from camera_opencv.msg import PositionValues
+from numpy import sign
 import time
 
 class Movement():
@@ -15,17 +16,36 @@ class Movement():
         self.camera_data.up_down = 0
         self.camera_data.left_right = 1
         self.camera_data.far_near = 2
-        self.move_on = 0   #moveOn_distance
-        self.theta = 0     #rotate_theta
+        self.theta = 0.0  #rotate_theta
+        self.go_back = 0.0    #moveOn_distance
+        self.move_theta = 0.0  #linear_x's value
+        self.move_on = 0.0     #augular_z's value
 
     def move(self):
-        target_time1 = 0.5 / 10
-        target_time2 = 1 / 10
-        target_time = (target_time1 + target_time2) / 2
+        target_time = 0.05
 
+        #acceleration processing
+        if self.theta == 0:
+            self.move_theta = self.move_theta - sign(self.move_theta) * 0.2  #sign
+        elif self.move_theta < 0 and self.move_theta >= self.theta:
+            self.move_theta = self.move_theta - 0.2
+        elif self.move_theta > 0 and self.move_theta <= self.theta:
+            self.move_theta = self.move_theta + 0.2
+        else:
+            self.move_theta = self.theta  #self.move_theta = 0
+        
+        if self.go_back == 0:
+            self.move_on = self.move_on - sign(self.move_on) * 0.05  #sign
+        elif self.move_on < 0 and self.move_on >= self.go_back:
+            self.move_on = self.move_on - 0.01
+        elif self.move_on > 0 and self.move_on <= self.go_back:
+            self.move_on = self.move_on + 0.01
+        else:
+            self.move_on = self.go_back  #self.move_on = 0
+        
         t = Twist()
         t.linear.x = (-1) * self.move_on * 0.2
-        t.angular.z = self.theta * 20 * 3.14 / 180
+        t.angular.z = self.move_theta * 20 * 3.14 / 180
 
         start_time = time.time()
         end_time = time.time()
@@ -64,20 +84,20 @@ class Movement():
             return
         
         if message.left_right == 0:
-            self.theta = -1
+            self.theta = -0.5
         elif message.left_right == 2:
-            self.theta = 1
+            self.theta = 0.5
         else:
-            self.theta = 0
+            self.theta = 0.0
 
         if message.far_near == 0:
-            self.move_on = 0.5
+            self.go_back = 0.5
         elif message.far_near == 1:
-            self.move_on = 0.5
+            self.go_back = 0.5
         elif message.far_near == 3:
-            self.move_on = -0.5
+            self.go_back = -0.5
         else:
-            self.move_on = 0
+            self.go_back = 0.0
         
         self.move()
 
